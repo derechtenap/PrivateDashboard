e5f0d94550d76f5e3b62837aaf0bcb752e86d72b
EricVanDeGriesel <55184120+EricVanDeGriesel@users.noreply.github.com>
2020-11-10 20:42:08 +0100

    Hourly and Daily Forecast
    Aktuelles Wetter muss angepasst werden. -> Anderer Api Call notwendig

--- weather.js
+++ weather.js
@@ -1,15 +1,9 @@
 var api = {
-    key:"16097ceeccf43bca442735334506a958",
-    base:"https://api.openweathermap.org/data/2.5/",
-    lat: "51.286473",
-    long: "7.679787"
+  key: "16097ceeccf43bca442735334506a958",
+  base: "https://api.openweathermap.org/data/2.5/",
+  lat: "51.286473",
+  long: "7.679787"
 }
-var searchbox = document.querySelector('.searchBox');
-if (searchbox){
-searchbox.addEventListener('keypress', setQuery);
-
-}
-
 /* Funktion falls man manuell Ort eingeben will bzw. über getLocation arbeiten will!
 function setQuery(evt){
     if (evt.keyCode == 13){
@@ -20,38 +14,48 @@ function setQuery(evt){
 }
 */
 
-function getResults(query){
-    fetch(`${api.base}onecall?lat=${api.lat}&lon=${api.long}&lang=de&units=metric&APPID=${api.key}`)
-        .then (weather => {
-            return weather.json();
-        }).then (displayResults);
+function getResults(query) {
+  fetch(`${api.base}onecall?lat=${api.lat}&lon=${api.long}&lang=de&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayWeatherWidgetFull);
+    
+
 }
 
 
-function displayResults(weather){
-    
-    console.log(weather);
-  
-    let icon = document.querySelector('.icon');
-    icon.innerHTML = "<img src=weatherIcons/"+weather.current.weather[0].icon +"@2x.png>";
+function displayWeatherWidgetFull(weather) {
+  displayResults(weather);
+  dailyForecast(weather);
+  hourlyForecast(weather)
+}
 
-    let temp = document.querySelector(' .temp');
-    temp.innerText = `${Math.round(weather.current.temp)}°C`;
 
-    let weather_el = document.querySelector(' .weather');
-    weather_el.innerText = weather.current.weather[0].description;
+function displayResults(weather) {
 
-    let feels = document.querySelector('.feels');
-    feels.innerText = `${Math.round(weather.current.feels_like)} °C`;
+  console.log(weather);
 
-    let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.daily[0].temp.min)}°C / ${Math.round(weather.daily[0].temp.max)}°C`;
+  let icon = document.querySelector('.icon');
+  icon.innerHTML = "<img src=weatherIcons/" + weather.current.weather[0].icon + "@2x.png>";
 
-    let hum = document.querySelector('.humidity');
-    hum.innerText = `${weather.current.humidity} %`;
+  let temp = document.querySelector(' .temp');
+  temp.innerText = `${Math.round(weather.current.temp)}°C`;
 
-    console.log(timeConverter(weather.daily[1].dt));
-    console.log(unixToDay(weather.daily[0].dt));
+  let weather_el = document.querySelector(' .weather');
+  weather_el.innerText = weather.current.weather[0].description;
+
+  let feels = document.querySelector('.feels');
+  feels.innerText = `${Math.round(weather.current.feels_like)} °C`;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.daily[0].temp.min)}°C / ${Math.round(weather.daily[0].temp.max)}°C`;
+
+  let hum = document.querySelector('.humidity');
+  hum.innerText = `${weather.current.humidity} %`;
+
+  console.log(timeConverter(weather.hourly[47].dt));
+  console.log(unixToDay(weather.daily[0].dt));
+  console.log(unixToHour(weather.daily[0].dt));
 }
 
 /* Aktuellen Standort abfragen. Jedoch zu ungenau für optimalen Erfolg aktuell
@@ -72,62 +76,69 @@ function getLocation() {
   }
 
 */
-  function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    //let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-   // let day = days[a.getDay()];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    //var hour = a.getHours();
-    //var min = a.getMinutes();
-    //var sec = a.getSeconds();
-    //var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    var time = date + ' ' + month + ' ' + year;
-    return time;
-  }
-  
-  let weekdays = ["Mo.","Di.","Mi.","Do.","Fr.","Sa.","So."];
-
-  function unixToDay(UnixDay){
-    var b = new Date(UnixDay*1000);
-    var day = weekdays[b.getDay()];
-
-    return day;
-  }
-
-
-  function weeklyForecast(weather){
-    for (let i = 1; i <= weather.daily.length; i++){
-      switch (unixToDay(weather.daily[i].dt)) {
-        case "Mo.":
-          
-          break;
-        case "Di.":
-
-          break;
-        case "Mi.":
-
-          break;
-        case "Do.":
-
-          break;
-        case "Fr.":
-
-          break;  
-        case "Sa.":
-
-          break;
-        case "So.":
-
-          break;
-      
-      }
-     
-   }
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[a.getDay()];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  //var time = date + ' ' + month + ' ' + year;
+  return time;
 }
 
+let weekdays = ["Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa.", "So."];
+
+function unixToDay(UnixDay) {
+  var b = new Date(UnixDay * 1000);
+  var day = weekdays[b.getDay()];
+
+  return day;
+}
+
+function unixToHour(UnixDay) {
+  var b = new Date(UnixDay * 1000);
+  var hour = b.getHours();
+  var minutes = b.getMinutes();
+
+  var time = hour + `:` + minutes + `0`;
+
+  return time;
+}
+
+
+function dailyForecast(weather) {
+  for (let i = 0; i < (weather.daily.length - 2); i++) {
+    var headerDay = document.getElementById("dailyHeaderSlot" + (i+1));
+    headerDay.innerText = unixToDay(weather.daily[i].dt);
+
+    var iconElement = document.getElementById("iconSlot" + (i+1));
+    iconElement.innerHTML = "<img src=weatherIcons/" + weather.daily[i].weather[0].icon + "@2x.png>";
+
+    var tempElement = document.getElementById("bodySlot" + (i+1));
+    tempElement.innerText = `${Math.round(weather.daily[i].temp.day)}°C`;
+
+  }
+}
+
+function hourlyForecast(weather) {
+  for (let i = 0; i < 6; i++){
+    var headerHourly = document.getElementById("hourlyHeaderSlot" + (i+1));
+    headerHourly.innerText = unixToHour(weather.hourly[i].dt);
+
+    var iconElementHourly = document.getElementById("hourlyIconSlot" + (i+1));
+    iconElementHourly.innerHTML = "<img src=weatherIcons/" + weather.hourly[i].weather[0].icon + "@2x.png>";
+
+    var tempElementHourly = document.getElementById("hourlyBodySlot" + (i+1));
+    tempElementHourly.innerText = `${Math.round(weather.hourly[i].temp)}°C`;
+  }
+
+}
 
 
 /*
