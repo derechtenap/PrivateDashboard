736da8d424f1fd665625768bfc437e69bda8a78b
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2020-11-15 03:09:10 +0100

    Fixes
    Bug fixes and function documentation

--- daily.widget.js
+++ daily.widget.js
@@ -1,17 +1,22 @@
 class WidgetDaily {
-    constructor() {
-        //
-    }
-    
+    /**
+     * create the clock time
+     */
     static setTime() {
         var clockElement = document.getElementById('clock');
     
         var now = new Date;
-        var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+        var h = this.addZero(now.getHours());
+        var m = this.addZero(now.getMinutes());
+        var s = this.addZero(now.getSeconds());
+        var time = h + ':' + m + ':' + s;
     
         clockElement.innerText = time;
     }
 
+    /**
+     * create the weekday and the dateday
+     */
     static setDate() {
         var weekdayElement = document.getElementById('weekday');
         var datedayElement = document.getElementById('dateday');
@@ -19,12 +24,29 @@ class WidgetDaily {
         var now = new Date;
         var weekdayNumber = now.getDay();
         var dateday = now.getUTCDate();
-        var weekday = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
+        var weekday = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
     
-        weekdayElement.innerText = weekday[weekdayNumber -1];
+        weekdayElement.innerText = weekday[weekdayNumber];
         datedayElement.innerText = dateday;
+    }
+
+    /**
+     * add a zero to a 2 digit value
+     * 
+     * @param {*} value 
+     */
+    static addZero(value) {
+        if (value < 10) {
+            value = "0" + value;
+        }
+
+        return value;
     }
 }
 
-setInterval(WidgetDaily.setTime, 100);
+WidgetDaily.setTime();
 WidgetDaily.setDate();
+
+setInterval(function () {
+    WidgetDaily.setTime();
+}, 1000);
